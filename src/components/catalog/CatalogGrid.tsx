77159eb38685d8670f2633900482d9fa71093685
KidstorePeru <kidstoreperu.pe@gmail.com>
2026-03-02 04:07:26 -0500

    Update CatalogGrid.tsx

--- src/components/catalog/CatalogGrid.tsx
+++ src/components/catalog/CatalogGrid.tsx
@@ -4,7 +4,7 @@ const categories = [
   { name: "Fortnite",         image: "/images/fortnite.jpg",  slug: "fortnite",          visible: true  },
   { name: "Wild Rift",        image: "/images/wildrift.jpg",  slug: "wild-rift",         visible: false },
   { name: "Marvel Rivals",    image: "/images/marvel.jpg",    slug: "marvel-rivals",     visible: false },
-  { name: "Pokémon GO",       image: "/images/pokemon.jpg",   slug: "pokemon-go",        visible: false },
+  { name: "Pokémon GO",       image: "/images/pokemon.jpg",   slug: "pokemon-go",        visible: true },
   { name: "Roblox",           image: "/images/roblox.jpg",    slug: "roblox",            visible: false },
   { name: "Genshin Impact",   image: "/images/genshin.jpg",   slug: "genshin-impact",    visible: false },
   { name: "Zenless Zone Zero",image: "/images/zzz.jpg",       slug: "zenless-zone-zero", visible: false },
